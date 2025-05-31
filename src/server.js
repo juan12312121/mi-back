860e04d9e29e61b2daf34110110426d18d0a77d6
RafaelAlvarez29 <rafa.alv.vgo@gmail.com>
2025-05-30 18:14:14 -0700

    a
    a

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@
 const app = require('./app');
 const sequelize = require('./config/config');
 require('dotenv').config();
+require('./models/rolesModel');    // Importa el modelo Rol
+require('./models/carrerasModel'); // Importa el modelo Carrera
+require('./models/gruposModel');   // Importa el modelo Grupo
+require('./models/usuarioModel'); // Importa el modelo Usuario (que depende de los anteriores)
+require('./models/materiasModel'); // Importa el modelo Carrera
+
 
 const PORT = process.env.PORT || 4000;
 
